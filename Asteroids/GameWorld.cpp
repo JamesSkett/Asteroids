b64039a9acfb4c9895c468d078d24b9e294eb931
James Skett <jamesskett31@gmail.com>
2024-02-27 20:17:58 +0000

    Added entity spawning and updating

--- Asteroids/GameWorld.cpp
+++ Asteroids/GameWorld.cpp
@@ -1,22 +1,97 @@
 #include "GameWorld.h"
+#include "Entity.h"
+#include "Ship.h"
 
 GameWorld::GameWorld(Window& window)
+    : m_window(window)
 {
 }
 
 bool GameWorld::Load()
 {
-    return false;
+    bool success = true;
+
+    // Check if assets load correctly
+    success &= m_resources.m_playerTex.loadFromFile("./assets/Ship.png");
+    success &= m_resources.m_asteroidTex.loadFromFile("./assets/Asteroid.png");
+
+    //success &= m_Resources.m_MainFont.loadFromFile("./assets/Fonts/RussoOne-Regular.ttf");
+
+    m_ship = SpawnEntity<Ship>();
+
+    // Return result of above statements
+    return true;
 }
 
 void GameWorld::Update(float dt)
 {
+	UpdateEntities(dt);
 }
 
-void GameWorld::Render()
+void GameWorld::Draw()
 {
+    sf::RenderWindow& rt = m_window.GetRenderWindow();
+
+    for (Entity* entity : m_entities)
+    {
+        if (entity != nullptr && !entity->IsDead())
+        {
+            entity->Draw(GetRenderTex());
+        }
+    }
+
+	//sf::Sprite fullScreenSprite(GetRenderTex().getTexture());
+
+	//const sf::Vector2f& vpSize = m_window.GetRenderWindow().getView().getSize();
+	//fullScreenSprite.setOrigin(static_cast<float>(vpSize.x) * Math::Half, static_cast<float>(vpSize.y) * Math::Half);
+
+	//fullScreenSprite.setScale(sf::Vector2f(1.0f, -1.0f));
+
+	//m_window.GetRenderWindow().draw(fullScreenSprite);
 }
 
 void GameWorld::Shutdown()
 {
+}
+
+void GameWorld::UpdateEntities(float dt)
+{
+	for (Entity* entity : m_entitiesToAdd) { m_entities.push_back(entity); }
+
+	m_entitiesToAdd.clear();
+
+	for (Entity* entity : m_entities)
+	{
+		if (entity != nullptr && !entity->IsDead())
+		{
+			entity->Update(dt);
+		}
+	}
+
+	for (auto it = m_entities.begin(); it < m_entities.end();)
+	{
+		if (Entity* entity = *it)
+		{
+			if (entity->IsDead())
+			{
+				entity->Destroy();
+
+				if (entity == m_ship)
+				{
+					m_ship = nullptr;
+				}
+
+				delete entity;
+				it = m_entities.erase(it);
+			}
+			else
+			{
+				++it;
+			}
+		}
+		else
+		{
+			it = m_entities.erase(it);
+		}
+	}
 }
--- Asteroids/GameWorld.h
+++ Asteroids/GameWorld.h
@@ -1,15 +1,18 @@
 #pragma once
 #include "Window.h"
 
+class Entity;
+class Ship;
+
 class GameWorld
 {
 public:
 	struct Resources
 	{
-		sf::Texture mPlayerTex;
-		sf::Texture	mAsteroidTex;
+		sf::Texture m_playerTex;
+		sf::Texture	m_asteroidTex;
 
-		sf::Font mMainFont;
+		sf::Font m_MainFont;
 	};
 
 	GameWorld(Window& window);
@@ -17,8 +20,39 @@ public:
 	bool Load();
 
 	void Update(float dt);
-	void Render();
+	void Draw();
 
 	void Shutdown();
+
+	sf::RenderWindow& GetRenderTex() const { return m_window.GetRenderWindow(); }
+	const Resources& GetResources() const { return m_resources; }
+
+	template<typename T>
+	T* SpawnEntity();
+
+private:
+
+	Resources m_resources;
+
+	Window& m_window;
+
+	Ship* m_ship = nullptr;
+	std::vector<Entity*> m_entities;
+	std::vector<Entity*> m_entitiesToAdd;
+
+	void UpdateEntities(float dt);
+
 };
 
+template<typename T>
+T* GameWorld::SpawnEntity()
+{
+	if (T* newEntity = new T(*this))
+	{
+		m_entitiesToAdd.push_back(newEntity);
+
+		return newEntity;
+	}
+
+	return nullptr;
+}
