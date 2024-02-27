6848b2551bd403554f8f8aadbe821b7b536b1899
James Skett <jamesskett31@gmail.com>
2024-02-27 20:16:00 +0000

    Added movement to ship class

--- Asteroids/Ship.cpp
+++ Asteroids/Ship.cpp
@@ -1,17 +1,73 @@
 #include "Ship.h"
+#include "GameWorld.h"
 
-Ship::Ship()
+Ship::Ship(GameWorld& world) 
+	: Entity(world, world.GetResources().m_playerTex)
 {
+	SetPosition(sf::Vector2f(400, 400));
+	GetSprite().setOrigin(GetSprite().getLocalBounds().width * Math::Half, GetSprite().getLocalBounds().height * Math::Half);
 }
 
 Ship::~Ship()
 {
 }
 
-void Ship::Update()
+void Ship::UpdateScore(int val)
+{
+	m_currentScore += val;
+}
+
+void Ship::OnUpdate(float dt)
+{
+	UpdateMovement(dt);
+
+
+}
+
+void Ship::OnRender(sf::RenderTexture& rt)
 {
 }
 
-void Ship::Draw()
+void Ship::OnDestroy()
 {
 }
+
+void Ship::UpdateMovement(float dt)
+{
+	sf::Vector2f accel(0.0f, 0.0f);
+
+	// Calculate thrust with possible adjustments based on SFML coordinate system
+	sf::Vector2f thrustDirection = sf::Vector2f(sin(Math::DegreesToRadians(m_rotation)), -cos(Math::DegreesToRadians(m_rotation)));
+	accel = (sf::Keyboard::isKeyPressed(sf::Keyboard::W) || sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) ? thrustDirection * s_accelerationSpeed : sf::Vector2f(0.f, 0.f);
+	
+
+
+	//if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) || sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+	//{
+	//	accel.y += 1.0f;
+	//}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) || sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+	{
+		m_rotation += 100.0f * dt;
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) || sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+	{
+		m_rotation -= 100.0f * dt;
+	}
+
+	if (Math::LengthSq(accel) > 0.0f)
+	{
+		accel = Math::Normalised(accel);
+		accel *= s_accelerationSpeed;
+
+		SetAcceleration(accel);
+	}
+	else
+	{
+		// Slow the player down by reversing velocity
+		SetAcceleration(GetVelocity() * -1.0f);
+	}
+}
+
--- Asteroids/Ship.h
+++ Asteroids/Ship.h
@@ -3,11 +3,37 @@
 class Ship : public Entity
 {
 public:
-	Ship();
+	Ship(GameWorld& world);
 	~Ship();
 
-	void Update();
-	void Draw();
+	int	  GetCurrentScore() const { return m_currentScore; }
+	void  UpdateScore(int val);
+
+protected:
+	void OnUpdate(float dt) override;
+	void OnRender(sf::RenderTexture& rt) override;
+
+	void OnDestroy() override;
+
+private:
+	// Movement
+
+	static constexpr float s_accelerationSpeed = 2000.0f;
+	static constexpr float s_maxSpeed = 750.0f;
+
+
+	// Gun
+	static constexpr float s_shootDelay = 0.1f;
+	float m_shootCooldown = 0.0f;
+
+	// Health
+
+	static constexpr int s_maxLives = 3;
+
+	// Score
+	int m_currentScore = 0;
+
+	void UpdateMovement(float dt);
 
 };
 
