226d5ea121b913b675ffc5fc62dc57a334ccd610
James Skett <jamesskett31@gmail.com>
2024-02-29 22:24:15 +0000

    Bullet now damages asteroid

--- Asteroids/Bullet.cpp
+++ Asteroids/Bullet.cpp
@@ -1,6 +1,7 @@
 #include "Bullet.h"
 #include "Ship.h"
 #include "GameWorld.h"
+#include "Asteroid.h"
 
 Bullet::Bullet(GameWorld& world)
 	: Entity(world, world.GetResources().m_bulletTex)
@@ -9,12 +10,12 @@ Bullet::Bullet(GameWorld& world)
 	SetMaxLives(1);
 	GetSprite().setScale(0.3f, 0.3f);
 	SetCanWrap(false);
+	GetSprite().setOrigin(GetSprite().getLocalBounds().width * Math::Half, GetSprite().getLocalBounds().height * Math::Half);
+	SetRadius(5.0f);
 }
 
 void Bullet::Launch(sf::Vector2f startPos, sf::Vector2f direction, const Entity* owner)
 {
-	//GetSprite().setColor(params.mColor);
-
 	SetPosition(startPos);
 	SetVelocity(Math::Normalised(direction) * sBulletSpeed);
 
@@ -25,16 +26,25 @@ void Bullet::OnCollision(Entity& other)
 {
 	if (&other != mOwner)
 	{
+		other.ApplyDamage(this, 1);
 		Kill();
 	}
 }
 
 void Bullet::OnUpdate(float dt)
 {
-
+	if (GetPosition().x + GetSprite().getLocalBounds().width * Math::Half < 0.0f)
+		Kill();
+	if (GetPosition().x > GetWorld().GetRenderWindow().getSize().x)
+		Kill();
+	if (GetPosition().y + GetSprite().getLocalBounds().height * Math::Half < 0.0f)
+		Kill();
+	if (GetPosition().y > GetWorld().GetRenderWindow().getSize().y)
+		Kill();
 }
 
 bool Bullet::CollidesWith(const Entity& other) const
 {
-	return &other != mOwner; //&& ((dynamic_cast<const Ship*>(&other) != nullptr) /*|| (dynamic_cast<const Enemy*>(&other) != nullptr)*/);
+	//Make sure the bullet does not collide with owner
+	return &other != mOwner && ((dynamic_cast<const Asteroid*>(&other) != nullptr) /*|| (dynamic_cast<const Enemy*>(&other) != nullptr)*/);
 }
