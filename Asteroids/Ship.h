54be70a01918f02260998133f4aea4b1bad14d0b
James Skett <jamesskett31@gmail.com>
2024-03-01 19:49:50 +0000

    Added alpha shift when in grace period

--- Asteroids/Ship.h
+++ Asteroids/Ship.h
@@ -20,17 +20,18 @@ protected:
 private:
 	// Movement
 
-	static constexpr float s_accelerationSpeed = 2000.0f;
-	static constexpr float s_maxSpeed = 750.0f;
-
+	const float m_accelerationSpeed = 1000.0f;
+	const float m_maxSpeed = 250.0f;
 
 	// Gun
-	static constexpr float s_shootDelay = 0.5f;
+	const float s_shootDelay = 0.5f;
 	float m_shootCooldown = 0.0f;
 
-	// Health
-
-	static constexpr int s_maxLives = 3;
+	//Respawn
+	int m_alphaDirection = -1;
+	const int m_minAlpha = 50;
+	const int m_alphaChangeSpeed = 200;
+	sf::Uint8 m_currentAlpha = 255;
 
 	// Score
 	int m_currentScore = 0;
